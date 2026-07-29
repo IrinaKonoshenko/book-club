34cafe994dcb4f1b9f7eecdb0393b452a787f3f0
Irina <103582958+IrinaKonoshenko@users.noreply.github.com>
2026-07-30 00:25:51 +0500

    feat: add girls data and type definition
    - Created a new file `girls.ts` in the data directory to store an array of girl objects, each with an id, title, date, image path, and description.
    - Added a new type definition for `Girls` in `types/girls.ts` to ensure type safety for the girl objects.

--- src/data/girls.ts
+++ src/data/girls.ts
@@ -1,65 +1,75 @@
-import type { Birthday } from "../types/birtday";
+import type { Girls } from "../types/girls";
 
-export const birthday: Birthday[] = [
+export const girls: Girls[] = [
    {
         id: 0,
         title: "Мэри",
         date: "15.01",
         img: "/girls/mary.jpg",
+        description: "",
     },
     {
         id: 1,
-        title: "Ирина Р",
+        title: "Ирина",
         date: "20.01",
-        img: "/girls/irinaR.jpg",
+        img: "/girls/irinaL.jpg",
+        description: "",
     },
     {
         id: 2,
         title: "Катя",
         date: "20.02",
         img: "/girls/katya.jpg",
+        description: "",
     },
     {
         id: 3,
         title: "Алекса",
         date: "09.04",
         img: "/girls/alex.jpg",
+        description: "",
     },
     {
         id: 4,
         title: "Жанна",
         date: "17.04",
         img: "/girls/janna1.jpg",
+        description: "",
     },
     {
         id: 5,
         title: "Натали",
         date: "06.06",
         img: "/girls/nata.jpg",
+        description: "",
     },
     {
         id: 6,
         title: "Камилла",
         date: "13.08",
         img: "/girls/kami2.jpg",
+        description: "",
     },
     {
         id: 7,
-        title: "Ирина К",
+        title: "Ирина",
         date: "04.09",
         img: "/girls/irinaK.jpg",
+        description: "",
     },
     {
         id: 8,
         title: "Баян",
         date: "15.11",
         img: "/girls/bayan2.jpg",
+        description: "",
     },
     {
         id: 9,
         title: "Юлдуз",
         date: "15.11",
         img: "/girls/ul3.jpg",
+        description: "",
     },
     
 ]
---
+++ src/types/girls.ts
@@ -0,0 +1,8 @@
+export interface Girls {
+    id: number;
+    title: string;
+    date: string;
+    img: string;
+    description: string;
+    
+}
